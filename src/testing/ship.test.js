7d4fd4152842cd14d72f2782667570564e90c925
Sam Hadfield <sthtech@protonmail.com>
2025-10-09 12:34:06 -0500

    feat: add test for isSunk fn

--- src/testing/ship.test.js
+++ src/testing/ship.test.js
@@ -1,7 +1,23 @@
 import { ship } from "../modules/ship.js";
 
-const newShip = ship(5, 0, false);
+const newShip = ship(2, 0, false);
+
+// test("this should increase the numOfHits by 1", () => {
+//   expect(newShip.hit()).toBe(0);
+// });
 
 test("this should increase the numOfHits by 1", () => {
   expect(newShip.hit()).toBe(1);
+});
+
+test("this should increase the numOfHits by 1", () => {
+  expect(newShip.hit()).toBe(2);
+});
+
+// test("this should increase the numOfHits by 1", () => {
+//   expect(newShip.hit()).toBe(3);
+// });
+
+test("this should report that the Destroyer has been sunk", () => {
+  expect(newShip.isSunk()).toBe("The vessel has been sunk!");
 });
