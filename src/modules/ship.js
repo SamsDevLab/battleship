a1e9b06accd3c5d5f2a2defb3df45d79a1003390
Sam Hadfield <sthtech@protonmail.com>
2025-10-09 12:11:47 -0500

    feat: add 'ship' factory function
    Add 'ship' factory fn that creates new object from ship's length,
    hitCount, and sinkStatus. Also includes hit fn that will increase the
    hitCount of the ship until it is sunk.

--- src/modules/ship.js
+++ src/modules/ship.js
@@ -1,1 +1,29 @@
-export const greeting = "Hello, World!";
+/* 
+Ship object needs to include:
+• Length (of ship)
+• Number of times ship has been hit
+• Whether or not ship has been sunk
+• Hit() function that increases number of 'hits' in your ship
+• isSunk() function - calculates whether a ship is considered sunk
+based on its length and the number of hits it has received
+
+Ship classes/sizes
+No.	Class of ship	Size
+1	Carrier	         5
+2	Battleship	     4
+3	Cruiser	         3
+4	Submarine	     3
+5	Destroyer	     2
+*/
+
+export const ship = (length, hitCount, sinkStatus) => {
+  return {
+    length: length,
+    hitCount: hitCount,
+    sinkStatus: sinkStatus,
+    hit: () => {
+      hitCount += 1;
+      return hitCount;
+    },
+  };
+};
