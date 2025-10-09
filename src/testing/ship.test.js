3e20f2fc01cd8124ce5dd1b2459ab4f50467d16c
Sam Hadfield <sthtech@protonmail.com>
2025-10-09 17:53:25 -0500

    fix: remove all args except 'length' from Ship() call

--- src/testing/ship.test.js
+++ src/testing/ship.test.js
@@ -1,6 +1,6 @@
 import { Ship } from "../modules/ship.js";
 
-const newShip = Ship(2, 0, false);
+const newShip = Ship(2);
 
 // test("this should increase the numOfHits by 1", () => {
 //   expect(newShip.hit()).toBe(0);
