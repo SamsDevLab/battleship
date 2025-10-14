ca6f4b97c2b7c94fa6d8da2d38720b7b5b20165f
Sam Hadfield <sthtech@protonmail.com>
2025-10-14 12:32:53 -0500

    fix: reorder tests, include single board instance
    Reorder testing to place them in order of checks within placeShip fn.
    Include a single board instantation which will be used to test boat
    coordinate placement as well as checking for already occupied squares on
    board. Previous tests created new board every time which prevented testing
    of already occupied squares

--- src/testing/gameboard.test.js
+++ src/testing/gameboard.test.js
@@ -7,99 +7,10 @@ const destroyerBoat = Ship(3);
 const battleshipBoat = Ship(4);
 const carrierBoat = Ship(5);
 
-// ********** Return Boat Coordinates: ************ //
-
-// Patrol:
-test("this should return the patrolBoat's coordinates", () => {
-  expect(Gameboard().placeShip(patrolBoat, 0, 0, "horizontal")).toEqual([
-    [0, 0],
-    [0, 1],
-  ]);
-});
-
-test("this should return the patrolBoat's coordinates", () => {
-  expect(Gameboard().placeShip(patrolBoat, 0, 0, "vertical")).toEqual([
-    [0, 0],
-    [1, 0],
-  ]);
-});
-
-// Submarine:
-test("this should return the submarineBoat's coordinates", () => {
-  expect(Gameboard().placeShip(submarineBoat, 0, 0, "horizontal")).toEqual([
-    [0, 0],
-    [0, 1],
-    [0, 2],
-  ]);
-});
-
-test("this should return the submarineBoat's coordinates", () => {
-  expect(Gameboard().placeShip(submarineBoat, 0, 0, "vertical")).toEqual([
-    [0, 0],
-    [1, 0],
-    [2, 0],
-  ]);
-});
-
-// Destroyer:
-test("this should return the destroyerBoat's coordinates", () => {
-  expect(Gameboard().placeShip(destroyerBoat, 0, 0, "horizontal")).toEqual([
-    [0, 0],
-    [0, 1],
-    [0, 2],
-  ]);
-});
-
-test("this should return the destroyerBoat's coordinates", () => {
-  expect(Gameboard().placeShip(destroyerBoat, 0, 0, "vertical")).toEqual([
-    [0, 0],
-    [1, 0],
-    [2, 0],
-  ]);
-});
-
-// Battleship:
-test("this should return the battleshipBoat's coordinates", () => {
-  expect(Gameboard().placeShip(battleshipBoat, 0, 0, "horizontal")).toEqual([
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-  ]);
-});
-
-test("this should return the battleshipBoat's coordinates", () => {
-  expect(Gameboard().placeShip(battleshipBoat, 0, 0, "vertical")).toEqual([
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [3, 0],
-  ]);
-});
-
-// Carrier:
-test("this should return the carrierBoat's coordinates", () => {
-  expect(Gameboard().placeShip(carrierBoat, 0, 0, "horizontal")).toEqual([
-    [0, 0],
-    [0, 1],
-    [0, 2],
-    [0, 3],
-    [0, 4],
-  ]);
-});
-
-test("this should return the carrierBoat's coordinates", () => {
-  expect(Gameboard().placeShip(carrierBoat, 0, 0, "vertical")).toEqual([
-    [0, 0],
-    [1, 0],
-    [2, 0],
-    [3, 0],
-    [4, 0],
-  ]);
-});
+const board = Gameboard();
 
 // ******************************** //
-// ** Testing for Negative Nums ** //
+// ** Check for Negative Values ** //
 // ****************************** //
 
 // Patrol
@@ -167,9 +78,9 @@ test("this should return an error, as starting nums cannot be negative", () => {
   );
 });
 
-// ************************ //
-// *** Can't Exceed 10 *** //
-// ********************** //
+// *******************************************************//
+// *** Check for Overflow (Can't Exceed 10 on Board) *** //
+// *****************************************************//
 
 // Patrol:
 test("this should return an error, as patrolBoat should not be placed outside board", () => {
@@ -235,3 +146,106 @@ test("this should return an error, as carrierBoat should not be placed outside b
     "Boat cannot be placed outside of gameboard",
   );
 });
+
+// // ************************************ //
+// // *** Check for Occupied Squares: *** //
+// // ********************************** //
+// test("this should return 'The ", () => {
+//   expect(board.placeShip(patrolBoat, 0, 0, "horizontal")).toEqual([
+//     [0, 0],
+//     [0, 1],
+//   ]);
+// });
+
+// ********************************* //
+// *** Return Boat Coordinates: *** //
+// ******************************* //
+
+// Patrol:
+test("this should return the patrolBoat's coordinates", () => {
+  expect(board.placeShip(patrolBoat, 0, 0, "horizontal")).toEqual([
+    [0, 0],
+    [0, 1],
+  ]);
+});
+
+test("this should return the patrolBoat's coordinates", () => {
+  expect(board.placeShip(patrolBoat, 1, 0, "vertical")).toEqual([
+    [1, 0],
+    [2, 0],
+  ]);
+});
+
+// Submarine:
+test("this should return the submarineBoat's coordinates", () => {
+  expect(board.placeShip(submarineBoat, 6, 0, "horizontal")).toEqual([
+    [6, 0],
+    [6, 1],
+    [6, 2],
+  ]);
+});
+
+test("this should return the submarineBoat's coordinates", () => {
+  expect(board.placeShip(submarineBoat, 7, 0, "vertical")).toEqual([
+    [7, 0],
+    [8, 0],
+    [9, 0],
+  ]);
+});
+
+// Destroyer:
+test("this should return the destroyerBoat's coordinates", () => {
+  expect(board.placeShip(destroyerBoat, 0, 4, "horizontal")).toEqual([
+    [0, 4],
+    [0, 5],
+    [0, 6],
+  ]);
+});
+
+test("this should return the destroyerBoat's coordinates", () => {
+  expect(board.placeShip(destroyerBoat, 1, 4, "vertical")).toEqual([
+    [1, 4],
+    [2, 4],
+    [3, 4],
+  ]);
+});
+
+// Battleship:
+test("this should return the battleshipBoat's coordinates", () => {
+  expect(board.placeShip(battleshipBoat, 5, 4, "horizontal")).toEqual([
+    [5, 4],
+    [5, 5],
+    [5, 6],
+    [5, 7],
+  ]);
+});
+
+test("this should return the battleshipBoat's coordinates", () => {
+  expect(board.placeShip(battleshipBoat, 6, 9, "vertical")).toEqual([
+    [6, 9],
+    [7, 9],
+    [8, 9],
+    [9, 9],
+  ]);
+});
+
+// Carrier:
+test("this should return the carrierBoat's coordinates", () => {
+  expect(board.placeShip(carrierBoat, 3, 5, "horizontal")).toEqual([
+    [3, 5],
+    [3, 6],
+    [3, 7],
+    [3, 8],
+    [3, 9],
+  ]);
+});
+
+// test("this should return the carrierBoat's coordinates", () => {
+//   expect(board.placeShip(carrierBoat, 5, 9, "vertical")).toEqual([
+//     [5, 9],
+//     [6, 9],
+//     [7, 9],
+//     [8, 9],
+//     [9, 9],
+//   ]);
+// });
