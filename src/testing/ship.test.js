09cda673b8c03df7cf4fd4639ae28e5ee3a2d32c
Sam Hadfield <sthtech@protonmail.com>
2025-10-15 12:26:34 -0500

    fix: remove extra tests
    Remove old test that tests a sinking boat as this is being tested elsewhere.

--- src/testing/ship.test.js
+++ src/testing/ship.test.js
@@ -2,22 +2,10 @@ import { Ship } from "../modules/ship.js";
 
 const newShip = Ship(2);
 
-// test("this should increase the numOfHits by 1", () => {
-//   expect(newShip.hit()).toBe(0);
-// });
-
 test("this should increase the numOfHits by 1", () => {
   expect(newShip.hit()).toBe(1);
 });
 
 test("this should increase the numOfHits by 1", () => {
   expect(newShip.hit()).toBe(2);
-});
-
-// test("this should increase the numOfHits by 1", () => {
-//   expect(newShip.hit()).toBe(3);
-// });
-
-test("this should report that the Destroyer has been sunk", () => {
-  expect(newShip.isSunk()).toBe("The vessel has been sunk!");
 });
