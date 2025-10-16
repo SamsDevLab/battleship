dfb83b65732adcea2f5391c4f71fcd1f8233a0c4
Sam Hadfield <sthtech@protonmail.com>
2025-10-16 16:14:16 -0500

    feat: add and export InitGame fn
    Import rules and logic modules and export InitGame fn to local index.js
    file

---
+++ src/modules/game-controller-and-ui/game-controller.js
@@ -0,0 +1,7 @@
+import { Gameboard } from "../game-controller-and-ui/index.js";
+import { Ship } from "../game-controller-and-ui/index.js";
+import { Player } from "../game-controller-and-ui/index.js";
+
+export const InitGame = () => {
+  console.log("ODB");
+};
