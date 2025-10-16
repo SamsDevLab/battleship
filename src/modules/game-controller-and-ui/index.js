c48f07ce78a3606257d9b3f9e039bf4f982359c0
Sam Hadfield <sthtech@protonmail.com>
2025-10-16 16:15:39 -0500

    feat: import rules/logic fns, export InitGame fn
    Import/export modules from game-rules-and-logic directory. Import/export
    InitGame module from game-controller.js

--- src/modules/game-controller-and-ui/index.js
+++ src/modules/game-controller-and-ui/index.js
@@ -1,1 +1,9 @@
-//
+import { Gameboard } from "../game-rules-and-logic/index.js";
+import { Ship } from "../game-rules-and-logic/index.js";
+import { Player } from "../game-rules-and-logic/index.js";
+import { InitGame } from "./game-controller.js";
+
+export { Gameboard };
+export { Ship };
+export { Player };
+export { InitGame };
