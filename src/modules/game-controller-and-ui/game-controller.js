24d0275f17417e6363a3f6c94b7f6ee617c7f2f8
Sam Hadfield <sthtech@protonmail.com>
2025-10-16 16:47:54 -0500

    feat: add players, boards, and ships for testing
    Add players (real and computer) and each player's board for testing purposes.
    Add ships to coordinates for each board in preparation for rendering
    everything to the DOM

--- src/modules/game-controller-and-ui/game-controller.js
+++ src/modules/game-controller-and-ui/game-controller.js
@@ -2,6 +2,53 @@ import { Gameboard } from "../game-controller-and-ui/index.js";
 import { Ship } from "../game-controller-and-ui/index.js";
 import { Player } from "../game-controller-and-ui/index.js";
 
+const patrolBoat = Ship("patrol", 2);
+const submarineBoat = Ship("submarine", 3);
+const destroyerBoat = Ship("destroyer", 3);
+const battleshipBoat = Ship("battleship", 4);
+const carrierBoat = Ship("carrier", 5);
+
 export const InitGame = () => {
-  console.log("ODB");
+  const realPlayer = Player().realPlayer("Sam's Dev Lab");
+  const computerPlayer = Player().computerPlayer();
+
+  const playerName = realPlayer.name;
+  const playerBoard = realPlayer.board;
+
+  playerBoard.placeShip(patrolBoat, 0, 4, "horizontal");
+  playerBoard.placeShip(submarineBoat, 0, 9, "vertical");
+  playerBoard.placeShip(destroyerBoat, 4, 2, "vertical");
+  playerBoard.placeShip(battleshipBoat, 7, 5, "horizontal");
+  playerBoard.placeShip(carrierBoat, 4, 0, "vertical");
+
+  const computerBoard = computerPlayer.board;
+
+  computerBoard.placeShip(patrolBoat, 0, 4, "horizontal");
+  computerBoard.placeShip(submarineBoat, 0, 9, "vertical");
+  computerBoard.placeShip(destroyerBoat, 4, 2, "vertical");
+  computerBoard.placeShip(battleshipBoat, 7, 5, "horizontal");
+  computerBoard.placeShip(carrierBoat, 4, 0, "vertical");
 };
+
+/*
+  Boats:
+  cB = carrierBoat | size = 5
+  bB = battleshipBoat | size = 4
+  dB = destroyerBoat | size = 3
+  sB = submarineBoat | size = 3
+  pb = patrolBoat | size = 2
+
+  [     0      1      2      3      4      5      6      7      8      9
+  0: [nulll, nulll, nulll, nulll, pBoat, pBoat, nulll, nulll, nulll, sBoat,]
+  1: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, sBoat,]
+  2: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, sBoat,]
+  3: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
+  4: [cBoat, nulll, dBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
+  5: [cBoat, nulll, dBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
+  6: [cBoat, nulll, dBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
+  7: [cBoat, nulll, nulll, nulll, nulll, bBoat, bBoat, bBoat, bBoat, nulll,]
+  8: [cBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
+  9: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
+  ]
+
+  */
