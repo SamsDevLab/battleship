5f7da34be69ea4da9b2ce3496e5c28109bf94c73
Sam Hadfield <sthtech@protonmail.com>
2025-10-16 14:57:14 -0500

    fix: file relocated from 'src/modules' directory

---
+++ src/modules/game-rules-and-logic/ship.js
@@ -0,0 +1,36 @@
+/* 
+Ship object needs to include:
+• Length (of ship)
+• Number of times ship has been hit
+• Whether or not ship has been sunk
+• Hit() function that increases number of 'hits' in your ship
+• isSunk() function - calculates whether a ship is considered sunk
+based on its length and the number of hits it has received
+
+Ship classes/sizes
+No.	Class of ship	Size
+1	Carrier	         5
+2	Battleship	     4
+3	Cruiser	         3
+4	Submarine	     3
+5	Destroyer	     2
+*/
+
+export const Ship = (name, length) => {
+  return {
+    name: name,
+    length: length,
+    hitCount: 0,
+    sinkStatus: false,
+    hit: function () {
+      this.hitCount += 1;
+      return this.hitCount;
+    },
+    isSunk: function () {
+      if (this.length === this.hitCount) {
+        this.sinkStatus = true;
+        return true;
+      } else return false;
+    },
+  };
+};
