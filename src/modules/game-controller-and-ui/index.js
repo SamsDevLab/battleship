d4bd5a44c25048217e57cecf89dfd92c6f02e8e7
Sam Hadfield <sthtech@protonmail.com>
2025-10-17 10:48:40 -0500

    feat: add RenderToDom import/export to index.js

--- src/modules/game-controller-and-ui/index.js
+++ src/modules/game-controller-and-ui/index.js
@@ -1,9 +1,11 @@
 import { Gameboard } from "../game-rules-and-logic/index.js";
 import { Ship } from "../game-rules-and-logic/index.js";
 import { Player } from "../game-rules-and-logic/index.js";
+import { RenderToDom } from "./dom.js";
 import { InitGame } from "./game-controller.js";
 
 export { Gameboard };
 export { Ship };
 export { Player };
 export { InitGame };
+export { RenderToDom };
