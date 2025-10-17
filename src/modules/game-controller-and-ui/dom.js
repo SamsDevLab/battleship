62b915cce87c349e0d568c5793185249fe19b80c
Sam Hadfield <sthtech@protonmail.com>
2025-10-17 10:54:43 -0500

    feat: include addRowsAndColumns helper fn
    Query the player board divs from the DOM and add rows and columns to the
    divs via addRowsAndColumns fn. Prepare for adding player data to the DOM

--- src/modules/game-controller-and-ui/dom.js
+++ src/modules/game-controller-and-ui/dom.js
@@ -1,1 +1,29 @@
-//
+import { InitGame } from "../game-controller-and-ui/index.js";
+
+const addRowsAndColumns = (boards) => {
+  const rows = 10;
+  const columns = 10;
+
+  boards.forEach((board) => {
+    for (let i = 0; i < rows; i++) {
+      const row = document.createElement("div");
+      row.classList.add("row");
+      board.append(row);
+      for (let j = 0; j < columns; j++) {
+        const column = document.createElement("div");
+        column.classList.add("column");
+        row.append(column);
+      }
+    }
+  });
+};
+
+export const RenderToDom = () => {
+  const players = InitGame();
+  const realPlayerBoard = players.realPlayerObj;
+  const computerPlayerBoard = players.computerPlayerObj;
+
+  const boards = document.querySelectorAll(".board");
+
+  addRowsAndColumns(boards);
+};
