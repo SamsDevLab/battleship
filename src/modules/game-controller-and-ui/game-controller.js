33cf12113cfc4cedb9a339e5d38868617df0b42b
Sam Hadfield <sthtech@protonmail.com>
2025-10-20 17:48:54 -0500

    refactor: attack() fn - include obj parameter
    Refactor attack() fn in GameController to include obj parameter. When
    a board is clicked, object will be passed in so that it's receiveAttack()
    can be accessed

--- src/modules/game-controller-and-ui/game-controller.js
+++ src/modules/game-controller-and-ui/game-controller.js
@@ -4,19 +4,18 @@ import { Player } from "../game-controller-and-ui/index.js";
 
 /*
 Responsibilities of Game Controller:
+• Click events in dom.js will pass coords to the GameController to make a 'shot' at those
+coords. If the space is not null, there will be a 'hit' on a boat. If the space IS 'null'
+the column will be marked as 'missed'
+  • After hit is made, the next turn should be made by the opposite player
+  • Will need some type of fn in place for computerPlayer so it can make a random hit
+    on the board
 
-Start here and with the DOM on Monday. 
-• You'll probably need to embed InitGame into the
-GameController which also means that the import/exports will change.
-• After figuring this out, you'll want to work on the event listeners for the buttons
-to begin dealing with the methods for the game logic
-
-
-• Add event listeners from buttons to manipulate game logic
 • DOM passes name input to GameController to give realPlayer a name??
   • Maybe does this through InitGame
+
 • DOM passes boat placement coords to GameController so GameController can utilize
-placeShip?
+placeShip ( this will happen when Drag and Drop system is in place)
 
 */
 
@@ -28,31 +27,37 @@ const carrierBoat = Ship("carrier", 5);
 
 // Second Draft:
 export const GameController = () => {
-  const buttons = document.querySelectorAll("button");
-  console.log(buttons);
-
   return {
     initGame: function () {
+      /*
+      This method will be refactored in the future as there will be a drag and drop system
+      from the DOM that will pass the coordinates to initGame to properly place the boats
+      */
+
       const realPlayerObj = Player().realPlayer("Sam's Dev Lab");
       const computerPlayerObj = Player().computerPlayer();
 
-      const playerBoard = realPlayerObj.board;
+      const playerGameMechanics = realPlayerObj.gameMechanics;
 
-      playerBoard.placeShip(patrolBoat, 0, 4, "horizontal");
-      playerBoard.placeShip(submarineBoat, 0, 9, "vertical");
-      playerBoard.placeShip(destroyerBoat, 4, 2, "vertical");
-      playerBoard.placeShip(battleshipBoat, 7, 5, "horizontal");
-      playerBoard.placeShip(carrierBoat, 4, 0, "vertical");
+      playerGameMechanics.placeShip(patrolBoat, 0, 4, "horizontal");
+      playerGameMechanics.placeShip(submarineBoat, 0, 9, "vertical");
+      playerGameMechanics.placeShip(destroyerBoat, 4, 2, "vertical");
+      playerGameMechanics.placeShip(battleshipBoat, 7, 5, "horizontal");
+      playerGameMechanics.placeShip(carrierBoat, 4, 0, "vertical");
 
-      const computerBoard = computerPlayerObj.board;
+      const computerGameMechanics = computerPlayerObj.gameMechanics;
 
-      computerBoard.placeShip(patrolBoat, 0, 4, "horizontal");
-      computerBoard.placeShip(submarineBoat, 0, 9, "vertical");
-      computerBoard.placeShip(destroyerBoat, 4, 2, "vertical");
-      computerBoard.placeShip(battleshipBoat, 7, 5, "horizontal");
-      computerBoard.placeShip(carrierBoat, 4, 0, "vertical");
+      computerGameMechanics.placeShip(patrolBoat, 0, 4, "horizontal");
+      computerGameMechanics.placeShip(submarineBoat, 0, 9, "vertical");
+      computerGameMechanics.placeShip(destroyerBoat, 4, 2, "vertical");
+      computerGameMechanics.placeShip(battleshipBoat, 7, 5, "horizontal");
+      computerGameMechanics.placeShip(carrierBoat, 4, 0, "vertical");
 
       return { realPlayerObj, computerPlayerObj };
+    },
+    attack: function (row, column, obj) {
+      const result = obj.gameMechanics.receiveAttack(row, column);
+      console.log(result);
     },
   };
 };
