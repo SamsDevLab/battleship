21a2345aec128257a82dc73c9570ab254529ad49
Sam Hadfield <sthtech@protonmail.com>
2025-10-21 09:26:34 -0500

    refactor: 'RenderToDom' plus helper fns
    Refactor 'RenderToDom' plus helper fns to better optimize data flow of
    objects and arrays

--- src/modules/game-controller-and-ui/dom.js
+++ src/modules/game-controller-and-ui/dom.js
@@ -22,10 +22,8 @@ Dom Responsibilites:
 
 const gameController = GameController();
 
-const renderPlayerBoardsToDom = (playerBoard, computerBoard) => {
-  const divs = Array.from(document.querySelectorAll("[data-board]"));
-  const playerDiv = divs[0];
-  const computerDiv = divs[1];
+const renderPlayerBoardsToDom = (playerObjs) => {
+  const playerDivs = Array.from(document.querySelectorAll("[data-board]"));
 
   const addRowsAndColumns = (board, div) => {
     for (let i = 0; i < board.length; i++) {
@@ -46,8 +44,14 @@ const renderPlayerBoardsToDom = (playerBoard, computerBoard) => {
     }
   };
 
-  addRowsAndColumns(playerBoard, playerDiv);
-  addRowsAndColumns(computerBoard, computerDiv);
+  addRowsAndColumns(
+    playerObjs.realPlayerObj.gameMechanics.board,
+    playerDivs[0],
+  );
+  addRowsAndColumns(
+    playerObjs.computerPlayerObj.gameMechanics.board,
+    playerDivs[1],
+  );
 };
 
 const addButtonFunctionality = (obj) => {
@@ -58,20 +62,34 @@ const addButtonFunctionality = (obj) => {
     const targetColumn = event.target.dataset.column;
 
     gameController.attack(targetRow, targetColumn, obj);
+
+    /* 
+    Start in this general area tomorrow (10/21)
+    
+    "For attacks, let the user click on a coordinate in the enemy Gameboard. 
+    Send the user input to methods on your objects, and re-render the boards to display the 
+    new information."
+
+    You'll need to re-render the board (here in dom.js) and probably disable the event
+    listener on that particular column. Also, maybe use a mock up color to denote that 
+    the cell has been clicked.
+
+    After this is done, you may want to mangage whose turn it is by creating a banner for both
+    the Player and the computer
+
+    And you'll have to start looking into how to manipulate the computer so that it chooses
+    a random attack on the player board
+    */
   });
 };
 
+// Second Draft:
 export const RenderToDom = () => {
-  const players = gameController.initGame();
-  const computerPlayerObj = players.computerPlayerObj;
+  const playerObjs = gameController.initGame();
 
-  console.log(players);
+  renderPlayerBoardsToDom(playerObjs);
 
-  const realPlayerBoard = players.realPlayerObj.gameMechanics.board;
-  const computerPlayerBoard = players.computerPlayerObj.gameMechanics.board;
-
-  renderPlayerBoardsToDom(realPlayerBoard, computerPlayerBoard);
-  addButtonFunctionality(computerPlayerObj);
+  addButtonFunctionality(playerObjs.computerPlayerObj);
 };
 
 /*
