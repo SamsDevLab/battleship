00da555a2daf255a0bd1a82807de111b22716d4b
Sam Hadfield <sthtech@protonmail.com>
2025-11-10 09:10:10 -0600

    docs: remove comments

--- src/modules/game-controller-and-ui/game-controller.js
+++ src/modules/game-controller-and-ui/game-controller.js
@@ -2,50 +2,12 @@ import { Gameboard } from "../game-controller-and-ui/index.js";
 import { Ship } from "../game-controller-and-ui/index.js";
 import { Player } from "../game-controller-and-ui/index.js";
 
-/*
-Responsibilities of Game Controller:
-• Click events in dom.js will pass coords to the GameController to make a 'shot' at those
-coords. If the space is not null, there will be a 'hit' on a boat. If the space IS 'null'
-the column will be marked as 'missed'
-  • After hit is made, the next turn should be made by the opposite player
-  • Will need some type of fn in place for computerPlayer so it can make a random hit
-    on the board
-
-• DOM passes name input to GameController to give realPlayer a name??
-  • Maybe does this through InitGame
-
-• DOM passes boat placement coords to GameController so GameController can utilize
-placeShip ( this will happen when Drag and Drop system is in place)
-
-*/
-
-// Second Draft:
 export const GameController = () => {
   return {
     initGame: function () {
-      /*
-      This method will be refactored in the future as there will be a drag and drop system
-      from the DOM that will pass the coordinates to initGame to properly place the boats
-      */
-      const realPlayerObj = Player().realPlayer("Sam's Dev Lab");
+      const realPlayerObj = Player().realPlayer();
       const computerPlayerObj = Player().computerPlayer();
-      // const playerGameMechanics = realPlayerObj.gameMechanics;
-      // playerGameMechanics.placeShip(Ship("patrol", 2), 0, 4, "horizontal");
-      // playerGameMechanics.placeShip(Ship("submarine", 3), 0, 9, "vertical");
-      // playerGameMechanics.placeShip(Ship("destroyer", 3), 4, 2, "vertical");
-      // playerGameMechanics.placeShip(Ship("battleship", 4), 7, 5, "horizontal");
-      // playerGameMechanics.placeShip(Ship("carrier", 5), 4, 0, "vertical");
-      // const computerGameMechanics = computerPlayerObj.gameMechanics;
-      // computerGameMechanics.placeShip(Ship("patrol", 2), 0, 4, "horizontal");
-      // computerGameMechanics.placeShip(Ship("submarine", 3), 0, 9, "vertical");
-      // computerGameMechanics.placeShip(Ship("destroyer", 3), 4, 2, "vertical");
-      // computerGameMechanics.placeShip(
-      //   Ship("battleship", 4),
-      //   7,
-      //   5,
-      //   "horizontal",
-      // );
-      // computerGameMechanics.placeShip(Ship("carrier", 5), 4, 0, "vertical");
+
       return { realPlayerObj, computerPlayerObj };
     },
     attack: function (row, column, opponentObj, attackerObj) {
@@ -61,26 +23,3 @@ export const GameController = () => {
 };
 
 GameController();
-
-/*
-  Boats:
-  cB = carrierBoat | size = 5
-  bB = battleshipBoat | size = 4
-  dB = destroyerBoat | size = 3
-  sB = submarineBoat | size = 3
-  pb = patrolBoat | size = 2
-
-  [     0      1      2      3      4      5      6      7      8      9
-  0: [nulll, nulll, nulll, nulll, pBoat, pBoat, nulll, nulll, nulll, sBoat,]
-  1: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, sBoat,]
-  2: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, sBoat,]
-  3: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
-  4: [cBoat, nulll, dBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
-  5: [cBoat, nulll, dBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
-  6: [cBoat, nulll, dBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
-  7: [cBoat, nulll, nulll, nulll, nulll, bBoat, bBoat, bBoat, bBoat, nulll,]
-  8: [cBoat, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
-  9: [nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll, nulll,]
-  ]
-
-  */
