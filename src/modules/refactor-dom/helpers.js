b7f54d2013e5b4351b31b5bdedb269107686cfb4
Sam Hadfield <sthtech@protonmail.com>
2025-11-17 16:28:21 -0600

    refactor: move querySelectors to helpers.js
    Move querySelectors from dom.js to helpers.js to reduce bloat of dom.js

---
+++ src/modules/refactor-dom/helpers.js
@@ -0,0 +1,30 @@
+export const boatPlacementContainer = document.querySelector(
+  "[data-container='boat-placement']",
+);
+export const boatContainersArr = Array.from(
+  document.querySelectorAll("[data-container='boat']"),
+);
+export const messageBanner = document.querySelector(
+  "[data-container='banner']",
+);
+export const playerDiv = document.querySelector("[data-board='player']");
+export const computerDiv = document.querySelector("[data-board='computer']");
+export const startScreen = document.querySelector(
+  "[data-modal='start-screen']",
+);
+export const startScreenBoard = document.querySelector(
+  "[data-board='start-screen']",
+);
+export const usernameAndButtonContainer = document.querySelector(
+  "[data-container='username-and-button']",
+);
+export let usernameInput = usernameAndButtonContainer.querySelector(
+  "[data-input='username']",
+);
+export const winnerScreen = document.querySelector(
+  "[data-modal='winner-screen']",
+);
+export const winnerHeader = document.querySelector("[data-winner-header]");
+export const playAgainButton = document.querySelector(
+  "[data-modal-button='play-again']",
+);
