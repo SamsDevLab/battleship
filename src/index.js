f540a9c2428eb2d8eec71312c5048cf71bc32db1
Sam Hadfield <sthtech@protonmail.com>
2025-11-18 09:44:32 -0600

    refactor: remove all extra code - abandoning mass refactor of dom.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
+// import { GameController } from "./modules/refactor-game/refactor-game-controller.js";
+// import { setPlayerObjs } from "./modules/refactor-state/refactor-ui-state.js";
+// import { openStartScreen } from "./modules/refactor-dom/renderer.js";
+// import { startScreenBoard } from "./modules/refactor-dom/helpers.js";
 import { RenderToDom } from "./modules/game-controller-and-ui/dom.js";
+
+// const gameController = GameController();
+// const playerObjs = gameController.initGame();
+// console.log(playerObjs);
+
+// setPlayerObjs(playerObjs);
+// openStartScreen(playerObjs.realPlayerObj.gameMechanics.board, startScreenBoard);
