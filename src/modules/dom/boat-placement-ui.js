6e22756d3022f52dc286fe272682d43270cfad67
Sam Hadfield <sthtech@protonmail.com>
2025-11-20 14:27:11 -0600

    refactor: encapsulate all logic withing BoatPlacement factory

--- src/modules/dom/boat-placement-ui.js
+++ src/modules/dom/boat-placement-ui.js
@@ -2,13 +2,7 @@ import { boatPlacementContainer } from "./dom-helpers";
 import { handleBoatInComputerArr } from "../game/ai-boat-placement";
 import { startGameButton } from "./dom-helpers";
 
-// const boatPlacementUI = () => {
-
-// }
-
-// ************************************
-// ********** Boat Selector ***********
-// ************************************
+export const BoatPlacement = () => {
 const selectBoat = (boatElement) => {
   boatElement.classList.remove("hover-effect");
   boatElement.classList.add("selected");
@@ -37,22 +31,6 @@ const checkForAlreadySelectedBoat = () => {
     alreadySelectedBoat.classList.add("hover-effect");
   }
 };
-
-const handleBoatContainerClick = (event) => {
-  const closestContainer = event.target.closest("[data-container]");
-
-  if (closestContainer.dataset.container === "boat") {
-    checkForAlreadySelectedBoat();
-    selectBoat(closestContainer);
-  } else if (closestContainer.dataset.container === "axis-button") {
-    const axisButton = closestContainer.querySelector("[data-button]");
-    toggleAxisButton(axisButton);
-  }
-};
-
-// ******************************************************************
-// ********** Handle Boat Placement on Start Screen Board ***********
-// ******************************************************************
 
 const getTargetColumns = () => {
   let targetRow = rows[currentBoat.row];
@@ -101,37 +79,10 @@ const highlightColumns = (targetColumnsArr) => {
   targetColumnsArr.forEach((column) => column.classList.add("highlight"));
 };
 
-export const handleHoverAddHighlight = (event) => {
-  if (event.target.dataset.board === "start-screen") return;
-
-  currentBoat.row = +event.target.closest("[data-row]").dataset.row;
-  currentBoat.column = +event.target.dataset.column;
-
-  const targetColumns = getTargetColumns();
-  const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
-
-  if (checkResult === true) return;
-
-  highlightColumns(targetColumns);
-};
-
 const removeHighlightSetColsAndRowsToNull = (targetColumnsArr) => {
   targetColumnsArr.forEach((column) => column.classList.remove("highlight"));
   currentBoat.row = null;
   currentBoat.column = null;
-};
-
-export const handleHoverRemoveHighlight = (event) => {
-  if (event.target.dataset.board === "start-screen") return;
-
-  currentBoat.row = +event.target.closest("[data-row]").dataset.row;
-
-  const targetColumns = getTargetColumns();
-
-  const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
-  if (checkResult === true) return;
-
-  removeHighlightSetColsAndRowsToNull(targetColumns);
 };
 
 const highlightColumnsAddRemovePointer = (targetColumnsArr) => {
@@ -149,29 +100,6 @@ const disableBoatContainer = () => {
   currentBoatContainer.classList.remove("selected");
   currentBoatContainer.classList.add("disabled");
 };
-
-const handleClickBoatSelectHighlight = () => {
-  if (currentBoat.name === "") return;
-
-  const targetColumns = getTargetColumns();
-
-  const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
-  if (checkResult === true) return;
-
-  placeBoatInPlayerArr();
-  handleBoatInComputerArr();
-  highlightColumnsAddRemovePointer(targetColumns);
-  disableBoatContainer();
-  setCurrentBoatToDefault();
-};
-
-// Username Input
-const handleInput = (event) => {
-  const inputValue = event.target.value;
-  realPlayerObj.name = inputValue;
-};
-
-// Start Battle Button
 
 const checkForAllBoatsPlaced = () => {
   const result = boatContainersArr.every((boatContainer) =>
@@ -191,6 +119,7 @@ const checkForErrorTag = () => {
   return result;
 };
 
+
 const insertErrorParagraphTag = () => {
   const checkResult = checkForErrorTag();
 
@@ -206,7 +135,62 @@ const insertErrorParagraphTag = () => {
   usernameAndButtonContainer.insertBefore(errorParagraphTag, startGameButton);
 };
 
-const handleStartButtonClick = () => {
+ return {
+ handleBoatContainerClick: function (event) {
+  const closestContainer = event.target.closest("[data-container]");
+
+  if (closestContainer.dataset.container === "boat") {
+    checkForAlreadySelectedBoat();
+    selectBoat(closestContainer);
+  } else if (closestContainer.dataset.container === "axis-button") {
+    const axisButton = closestContainer.querySelector("[data-button]");
+    toggleAxisButton(axisButton);
+  }
+},
+  handleHoverAddHighlight: function (event) {
+  if (event.target.dataset.board === "start-screen") return;
+
+  currentBoat.row = +event.target.closest("[data-row]").dataset.row;
+  currentBoat.column = +event.target.dataset.column;
+
+  const targetColumns = getTargetColumns();
+  const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
+
+  if (checkResult === true) return;
+
+  highlightColumns(targetColumns);
+},
+  handleHoverRemoveHighlight: function (event) {
+  if (event.target.dataset.board === "start-screen") return;
+
+  currentBoat.row = +event.target.closest("[data-row]").dataset.row;
+
+  const targetColumns = getTargetColumns();
+
+  const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
+  if (checkResult === true) return;
+
+  removeHighlightSetColsAndRowsToNull(targetColumns);
+},
+ handleClickBoatSelectHighlight: function () {
+  if (currentBoat.name === "") return;
+
+  const targetColumns = getTargetColumns();
+
+  const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
+  if (checkResult === true) return;
+
+  placeBoatInPlayerArr();
+  handleBoatInComputerArr();
+  highlightColumnsAddRemovePointer(targetColumns);
+  disableBoatContainer();
+  setCurrentBoatToDefault();
+},
+ handleInput: function(event) {
+  const inputValue = event.target.value;
+  realPlayerObj.name = inputValue;
+},
+   handleStartButtonClick: function () => {
   const boatPlacementResult = checkForAllBoatsPlaced();
   const playerNamePlacement = checkForPlayerNamePlacement();
 
@@ -218,4 +202,221 @@ const handleStartButtonClick = () => {
     messageBanner.textContent = `${realPlayerObj.name} shoots first!`;
     RenderToDom();
   }
+   }
+ },
 };
+
+// ************************************
+// ********** Boat Selector ***********
+// ************************************
+
+// const selectBoat = (boatElement) => {
+//   boatElement.classList.remove("hover-effect");
+//   boatElement.classList.add("selected");
+
+//   currentBoat.name = boatElement.dataset.boatName;
+//   currentBoat.length = +boatElement.dataset.boatLength;
+// };
+
+// const toggleAxisButton = (button) => {
+//   if (currentBoat.direction === "horizontal") {
+//     button.textContent = "Vertical Axis";
+//     currentBoat.direction = "vertical";
+//   } else if (currentBoat.direction === "vertical") {
+//     button.textContent = "Horizontal Axis";
+//     currentBoat.direction = "horizontal";
+//   }
+// };
+
+// const checkForAlreadySelectedBoat = () => {
+//   const alreadySelectedBoat = boatContainersArr.find((boatContainer) =>
+//     boatContainer.classList.contains("selected"),
+//   );
+
+//   if (alreadySelectedBoat !== undefined) {
+//     alreadySelectedBoat.classList.remove("selected");
+//     alreadySelectedBoat.classList.add("hover-effect");
+//   }
+// };
+
+// export const handleBoatContainerClick = (event) => {
+//   const closestContainer = event.target.closest("[data-container]");
+
+//   if (closestContainer.dataset.container === "boat") {
+//     checkForAlreadySelectedBoat();
+//     selectBoat(closestContainer);
+//   } else if (closestContainer.dataset.container === "axis-button") {
+//     const axisButton = closestContainer.querySelector("[data-button]");
+//     toggleAxisButton(axisButton);
+//   }
+// };
+
+// ******************************************************************
+// ********** Handle Boat Placement on Start Screen Board ***********
+// ******************************************************************
+
+// const getTargetColumns = () => {
+//   let targetRow = rows[currentBoat.row];
+//   let columns = targetRow.children;
+//   let targetColumn = columns[currentBoat.column];
+//   const finalTargetColumns = [];
+
+//   if (currentBoat.direction === "horizontal") {
+//     for (let i = 0; i < currentBoat.length; i++) {
+//       targetColumn = columns[currentBoat.column + i];
+//       finalTargetColumns.push(targetColumn);
+//     }
+//   } else if (currentBoat.direction === "vertical") {
+//     for (let i = 0; i < currentBoat.length; i++) {
+//       targetRow = rows[currentBoat.row + i];
+//       if (targetRow === undefined) {
+//         finalTargetColumns.push(targetRow);
+//       } else {
+//         columns = targetRow.children;
+//         targetColumn = columns[currentBoat.column];
+//         finalTargetColumns.push(targetColumn);
+//       }
+//     }
+//   }
+
+//   return finalTargetColumns;
+// };
+
+// const checkForUndefinedAndRemovePointerClass = (targetColumnsArr) => {
+//   const checkForUndefined = targetColumnsArr.some(
+//     (column) => column === undefined,
+//   );
+
+//   if (checkForUndefined === true) return true;
+
+//   const checkForRemovePointer = targetColumnsArr.some((column) =>
+//     column.classList.contains("remove-pointer"),
+//   );
+
+//   if (checkForRemovePointer === true) return true;
+
+//   return false;
+// };
+
+// const highlightColumns = (targetColumnsArr) => {
+//   targetColumnsArr.forEach((column) => column.classList.add("highlight"));
+// };
+
+// export const handleHoverAddHighlight = (event) => {
+//   if (event.target.dataset.board === "start-screen") return;
+
+//   currentBoat.row = +event.target.closest("[data-row]").dataset.row;
+//   currentBoat.column = +event.target.dataset.column;
+
+//   const targetColumns = getTargetColumns();
+//   const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
+
+//   if (checkResult === true) return;
+
+//   highlightColumns(targetColumns);
+// };
+
+// const removeHighlightSetColsAndRowsToNull = (targetColumnsArr) => {
+//   targetColumnsArr.forEach((column) => column.classList.remove("highlight"));
+//   currentBoat.row = null;
+//   currentBoat.column = null;
+// };
+
+// export const handleHoverRemoveHighlight = (event) => {
+//   if (event.target.dataset.board === "start-screen") return;
+
+//   currentBoat.row = +event.target.closest("[data-row]").dataset.row;
+
+//   const targetColumns = getTargetColumns();
+
+//   const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
+//   if (checkResult === true) return;
+
+//   removeHighlightSetColsAndRowsToNull(targetColumns);
+// };
+
+// const highlightColumnsAddRemovePointer = (targetColumnsArr) => {
+//   targetColumnsArr.forEach((column) => {
+//     column.classList.add("disabled");
+//     column.classList.add("remove-pointer");
+//   });
+// };
+
+// const disableBoatContainer = () => {
+//   const currentBoatContainer = boatContainersArr.find((container) =>
+//     container.classList.contains("selected"),
+//   );
+
+//   currentBoatContainer.classList.remove("selected");
+//   currentBoatContainer.classList.add("disabled");
+// };
+
+// const handleClickBoatSelectHighlight = () => {
+//   if (currentBoat.name === "") return;
+
+//   const targetColumns = getTargetColumns();
+
+//   const checkResult = checkForUndefinedAndRemovePointerClass(targetColumns);
+//   if (checkResult === true) return;
+
+//   placeBoatInPlayerArr();
+//   handleBoatInComputerArr();
+//   highlightColumnsAddRemovePointer(targetColumns);
+//   disableBoatContainer();
+//   setCurrentBoatToDefault();
+// };
+
+// Username Input
+// const handleInput = (event) => {
+//   const inputValue = event.target.value;
+//   realPlayerObj.name = inputValue;
+// };
+
+// Start Battle Button
+
+// const checkForAllBoatsPlaced = () => {
+//   const result = boatContainersArr.every((boatContainer) =>
+//     boatContainer.classList.contains("disabled"),
+//   );
+
+//   return result;
+// };
+
+// const checkForPlayerNamePlacement = () => {
+//   if (realPlayerObj.name === undefined) return false;
+//   else return true;
+// };
+
+// const checkForErrorTag = () => {
+//   const result = document.querySelector("[data-tag='error']");
+//   return result;
+// };
+
+// const insertErrorParagraphTag = () => {
+//   const checkResult = checkForErrorTag();
+
+//   if (checkResult !== null) return;
+
+//   const errorParagraphTag = document.createElement("p");
+//   errorParagraphTag.textContent =
+//     "⚠️ Please enter your name and place all 5 of your ships!";
+
+//   errorParagraphTag.id = "error-tag";
+//   errorParagraphTag.dataset.tag = "error";
+
+//   usernameAndButtonContainer.insertBefore(errorParagraphTag, startGameButton);
+// };
+
+// const handleStartButtonClick = () => {
+//   const boatPlacementResult = checkForAllBoatsPlaced();
+//   const playerNamePlacement = checkForPlayerNamePlacement();
+
+//   if (boatPlacementResult === false || playerNamePlacement === false) {
+//     insertErrorParagraphTag();
+//   } else {
+//     startScreen.classList.remove("is-open");
+//     startScreen.close();
+//     messageBanner.textContent = `${realPlayerObj.name} shoots first!`;
+//     RenderToDom();
+//   }
+// };
