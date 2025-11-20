a88b92388638121da1c20861bd9a190fbfa9907b
Sam Hadfield <sthtech@protonmail.com>
2025-11-20 14:07:14 -0600

    refactor: place methods into a factory fn

--- src/modules/state/ui-state.js
+++ src/modules/state/ui-state.js
@@ -1,15 +1,20 @@
-let currentBoat = {
-  direction: "horizontal",
-  name: "",
-  length: 0,
-  row: 0,
-  column: 0,
-};
+import { startScreen } from "../dom/dom-helpers";
 
-const setCurrentBoatToDefault = () => {
-  currentBoat.direction = "horizontal";
-  currentBoat.name = "";
-  currentBoat.length = 0;
-  currentBoat.row = 0;
-  currentBoat.column = 0;
+export const State = () => {
+  let currentBoat = {
+    direction: "horizontal",
+    name: "",
+    length: 0,
+    row: 0,
+    column: 0,
+  };
+  return {
+    setCurrentBoatToDefault: function () {
+      currentBoat.direction = "horizontal";
+      currentBoat.name = "";
+      currentBoat.length = 0;
+      currentBoat.row = 0;
+      currentBoat.column = 0;
+    },
+  };
 };
