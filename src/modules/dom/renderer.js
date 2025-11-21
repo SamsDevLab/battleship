77c67a6f608e3ff9872063658ca7d49f82a6c206
Sam Hadfield <sthtech@protonmail.com>
2025-11-21 10:09:32 -0600

    refactor: pass startScreen param, mv fn to helper
    Pass in startScreen parameter to openStartScreen. Previously, was attempting
    to access startScreen within openStartScreen fn by using a startScreen
    import.
    
    Also, move addRowsAndColumns to helper function rather than a return
    function. For now, it doesn't seem to be directly invoked in any other module

--- src/modules/dom/renderer.js
+++ src/modules/dom/renderer.js
@@ -1,6 +1,28 @@
-import { startScreen } from "./dom-helpers";
-
 export const Renderer = () => {
+  const addRowsAndColumns = (board, div) => {
+    const competitorBoard = div.dataset.board;
+
+    for (let i = 0; i < board.length; i++) {
+      const boardRow = document.createElement("div");
+      boardRow.classList.add("row");
+      boardRow.dataset.row = `${i}`;
+      div.append(boardRow);
+
+      for (let j = 0; j < board.length; j++) {
+        const boardColumn = document.createElement("button");
+        boardColumn.classList.add("column");
+        boardColumn.dataset.column = `${j}`;
+
+        if (board[i][j] === "missed" || board[i][j] === "hit") {
+          markPreviousAttackOnBoard(board[i][j], boardColumn);
+        } else if (board[i][j] !== null && competitorBoard !== "computer") {
+          boardColumn.classList.add("highlight");
+        }
+        boardRow.append(boardColumn);
+      }
+    }
+  };
+
   return {
     markPreviousAttackOnBoard: function (attack, boardColumn) {
       const markAttackSpan = document.createElement("span");
@@ -9,30 +31,7 @@ export const Renderer = () => {
       boardColumn.append(markAttackSpan);
       boardColumn.classList.add("remove-pointer");
     },
-    addRowsAndColumns: function (board, div) {
-      const competitorBoard = div.dataset.board;
-
-      for (let i = 0; i < board.length; i++) {
-        const boardRow = document.createElement("div");
-        boardRow.classList.add("row");
-        boardRow.dataset.row = `${i}`;
-        div.append(boardRow);
-
-        for (let j = 0; j < board.length; j++) {
-          const boardColumn = document.createElement("button");
-          boardColumn.classList.add("column");
-          boardColumn.dataset.column = `${j}`;
-
-          if (board[i][j] === "missed" || board[i][j] === "hit") {
-            markPreviousAttackOnBoard(board[i][j], boardColumn);
-          } else if (board[i][j] !== null && competitorBoard !== "computer") {
-            boardColumn.classList.add("highlight");
-          }
-          boardRow.append(boardColumn);
-        }
-      }
-    },
-    openStartScreen: function (realPlayerBoard, screenBoard) {
+    openStartScreen: function (realPlayerBoard, screenBoard, startScreen) {
       startScreen.classList.add("is-open");
       startScreen.showModal();
       screenBoard.replaceChildren();
