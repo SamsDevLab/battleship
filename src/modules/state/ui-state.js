c824486ba707e26681c4786e85242510c85340e6
Sam Hadfield <sthtech@protonmail.com>
2025-11-21 15:10:13 -0600

    feat: add 'getters' and 'setters' for currentBoat
    Add 'getters' and 'setters' to ui-state.js to manipulate currentBoat
    object. Previously, was attempting to change currentBoat within various
    modules but, by calling 'State', was just making different instances

--- src/modules/state/ui-state.js
+++ src/modules/state/ui-state.js
@@ -1,4 +1,6 @@
 export const State = () => {
+  let realPlayer;
+  let computerPlayer;
   let currentBoat = {
     direction: "horizontal",
     name: "",
@@ -7,13 +9,37 @@ export const State = () => {
     column: 0,
   };
   return {
-    currentBoat,
+    getRealPlayer: function () {
+      return realPlayer;
+    },
+    getComputerPlayer: function () {
+      return computerPlayer;
+    },
+    getCurrentBoat: function () {
+      return currentBoat;
+    },
+    storeObjects: function (playerObjs) {
+      realPlayer = playerObjs.realPlayerObj;
+      computerPlayer = playerObjs.computerPlayer;
+    },
     setCurrentBoatToDefault: function () {
       currentBoat.direction = "horizontal";
       currentBoat.name = "";
       currentBoat.length = 0;
       currentBoat.row = 0;
       currentBoat.column = 0;
+    },
+    setCurrentBoatName: function (name) {
+      currentBoat.name = name;
+    },
+    setCurrentBoatLength: function (length) {
+      currentBoat.length = length;
+    },
+    setCurrentBoatRow: function (row) {
+      currentBoat.row = row;
+    },
+    setCurrentBoatColumn: function (column) {
+      currentBoat.column = column;
     },
   };
 };
