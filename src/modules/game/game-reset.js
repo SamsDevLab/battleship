01f35d92ca2638837c58814aa2007994cdf4308c
Sam Hadfield <sthtech@protonmail.com>
2025-11-25 14:27:08 -0600

    refactor: migrate all reset logic to game-reset.js
    Migrate all game reset logic from dom.js to game-reset.js

---
+++ src/modules/game/game-reset.js
@@ -0,0 +1,63 @@
+export const GameReset = (domHelpers, gameController, state, renderer) => {
+  const resetGameBoards = () => {
+    domHelpers.playerDiv.replaceChildren();
+    domHelpers.computerDiv.replaceChildren();
+  };
+
+  const resetPlayerObjs = () => {
+    state.setPlayerObjsToDefault();
+    // playerObjs = null;
+    const playerObjs = gameController.initGame();
+    state.storeObjects(playerObjs);
+    // realPlayerObj = playerObjs.realPlayerObj;
+    // computerPlayerObj = playerObjs.computerPlayerObj;
+  };
+
+  const resetBoatContainers = () => {
+    domHelpers.boatContainersArr.forEach((boatContainer) => {
+      boatContainer.classList.remove("disabled");
+      boatContainer.classList.add("hover-effect");
+    });
+  };
+
+  const resetStartScreenColumns = () => {
+    const highlightedColumns = Array.from(
+      domHelpers.startScreenBoard.querySelectorAll(".highlight"),
+    );
+    highlightedColumns.forEach((column) => {
+      column.classList.remove("highlight");
+      column.classList.remove("disabled");
+    });
+  };
+
+  const resetUsernameInput = () => {
+    domHelpers.usernameInput.value = "";
+  };
+
+  const resetIncompleteWarning = () => {
+    // const errorTag = document.querySelector("[data-tag='error']");
+    if (domHelpers.errorTag === null) {
+      return;
+    } else domHelpers.errorTag.remove();
+  };
+
+  return {
+    handleGameReset: function () {
+      domHelpers.winnerScreen.classList.remove("show-winner");
+      domHelpers.winnerScreen.close();
+      resetGameBoards();
+      resetPlayerObjs();
+      resetBoatContainers();
+      resetStartScreenColumns();
+      resetUsernameInput();
+      resetIncompleteWarning();
+
+      const realPlayer = state.getRealPlayer();
+      renderer.openStartScreen(
+        realPlayer.gameMechanics.board,
+        domHelpers.startScreenBoard,
+        domHelpers.startScreen,
+      );
+    },
+  };
+};
